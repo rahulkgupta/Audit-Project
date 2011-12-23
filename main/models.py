219824f6ebc824fc96e119db84b4b2456c69ff47
Krishna Kalpathy <krishna.jk@gmail.com>
2011-12-22 18:31:35 -0800

    models for users and groups

--- main/models.py
+++ main/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class UserProfile (Models.model):
+    user = models.OneToOneField(User)
+    title = models.CharField(max_length=30)
+    org = CharField(max_length=20)
+    suborg = models.CharField(max_length=20)
+
+class Org (Models.model):
+    name = models.CharField(max_length=20)
+    
+    ORG_TYPES = (
+        ('EDU', 'Educational Institution').
+        ('COM', 'Large Corporation'),
+        ('SMB', 'Small Business or Company')
+        ('STG', 'Student Group'),
+        ('DPT', 'Department')
+    )
+
+    type = models.CharField(max_length=3, choices=ORG_TYPES)
+    parent = models.ForeignKey(Org)
+    members = models.ManyToManyField(UserProfile)
+
+#class assocs (Models.model):
+#    user = models.ForeignKey(UserProfile)
+#    org = models.(Org)
+        
 # Create your models here.
